4cb04bc4eb54b22e6fd9ac207cf35ed74dd3a34b
raja reddy <rajareddy9526@gmail.com>
2022-09-06 05:14:34 +0000

    Blog List

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import './index.css'
+
+import BlogItem from '../BlogItem'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <div className="blog-list-container">
+      {blogsList.map(item => (
+        <BlogItem key={item.id} blog={item} />
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
